74aa5cf20c986a5d64a475bf0bd46fcabd263680
Pavel Bereza <rabotam2013@yandex.ru>
2024-06-09 16:36:11 +0300

    1 Commit update file .env

--- arguments_passing/filters/filters.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters import BaseFilter
-from aiogram.types import TelegramObject
-
-
-class MyTrueFilter(BaseFilter):
-
-    async def __call__(self, event: TelegramObject, my_int_var, my_text_var) -> bool:
-        print('Попали внутрь фильтра', my_int_var, my_text_var)
-        return True
